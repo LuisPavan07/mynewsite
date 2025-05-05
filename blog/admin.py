70838b294f5793d05c117e8f35484e890549a368
LuisPavan07 <luihoracio@gmail.com>
2025-05-05 15:19:36 -0300

    Add Post model to Django admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on', 'status')
+    search_fields = ('title', 'content')
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
